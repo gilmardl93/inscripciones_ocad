6d2a96f75fd74233101a70f78727957ad1af71dc
Luis Mayta <luis.mayta@gmail.com>
2017-06-16 15:19:33 -0500

    Se agrego dependencia faltante

--- app/Http/Controllers/Admin/Aulas/AulasController.php
+++ app/Http/Controllers/Admin/Aulas/AulasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin\Aulas;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\CreateAulaRequest;
 use App\Models\Aula;
 use DB;
 use Illuminate\Http\Request;
